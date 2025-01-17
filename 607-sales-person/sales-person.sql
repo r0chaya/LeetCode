d2ceae5c51ed735df5134afab0a9b6b2307e7483
r0chaya <119661388+r0chaya@users.noreply.github.com>
2025-01-17 10:37:06 +0700

    Time: 2616 ms (22.37%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -3,7 +3,7 @@ FROM SalesPerson s
 WHERE s.name NOT IN (
     SELECT s1.name
     FROM SalesPerson s1
-    JOIN Orders o ON s1.sales_id = o.sales_id
-    JOIN Company c ON o.com_id = c.com_id
+    JOIN Orders USING (sales_id)
+    JOIN Company c USING (com_id)
     WHERE c.name = 'RED'
 );
